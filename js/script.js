57e8b47b00dde9ba5ec16af62c8bcf1d7eafa641
Xamrayev Xasan <xamrayevxasan11@gmail.com>
2024-04-04 16:56:24 +0500

    finished

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// let body = document.querySelector("body");
+// let div = document.createElement("div");
+// body.appendChild(div);
+
+// // arrayni uziga qushish
+
+// let arr = [
+//   [1, 2],
+//   [1, 2],
+// ];
+// let totalNum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr[i].length; j++) {
+//     totalNum += arr[i][j];
+//   }
+// }
+
+// div.innerHTML = totalNum;
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+/////////////// 1 topshiriq
+
+// my_list = ["home", "joke", "laptop", "table", "chair", "book"];
+// last_element_length = my_list[my_list.length - 1].length;
+// console.log(last_element_length);
+
+////////////// 2 topshiriq
+
+// input_string = "1.1.1.2.3.4;";
+// output_string = input_string.replaceAll(".", "[.]");
+// console.log(output_string);
+
+/////////////// 3 topshiriq
+
+// let obj = {
+//   1: "Home",
+//   2: "Joke",
+//   3: "Laptop",
+//   4: "Table",
+// };
+
+// let allKeys = Object.keys(obj).length;
+// console.log(allKeys);
+
+
+
+//////////////// 4 topshiriq
+
+
+
+// function factorial(n) {
+//   return n != 1 ? n * factorial(n - 1) : 1;
+// }
+
+// alert(factorial(5));
